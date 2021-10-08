a0559cbba3b74a57e5cf2a6e196fa9f4233e78a8
Guangzhong Chen <guangzhong0@gmail.com>
2021-10-08 13:13:14 +0100

    Loaded the pacman code and completed GoWestAgent

---
+++ pacman/sampleAgents.py
@@ -0,0 +1,162 @@
+# sampleAgents.py
+# parsons/07-oct-2017
+#
+# Version 1.1
+#
+# Some simple agents to work with the PacMan AI projects from:
+#
+# http://ai.berkeley.edu/
+#
+# These use a simple API that allow us to control Pacman's interaction with
+# the environment adding a layer on top of the AI Berkeley code.
+#
+# As required by the licensing agreement for the PacMan AI we have:
+#
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+# The agents here are extensions written by Simon Parsons, based on the code in
+# pacmanAgents.py
+
+from pacman import Directions
+from game import Agent
+import api
+import random
+import game
+import util
+
+# RandomAgent
+#
+# A very simple agent. Just makes a random pick every time that it is
+# asked for an action.
+class RandomAgent(Agent):
+
+    def getAction(self, state):
+        # Get the actions we can try, and remove "STOP" if that is one of them.
+        legal = api.legalActions(state)
+        if Directions.STOP in legal:
+            legal.remove(Directions.STOP)
+        # Random choice between the legal options.
+        return api.makeMove(random.choice(legal), legal)
+
+# RandomishAgent
+#
+# A tiny bit more sophisticated. Having picked a direction, keep going
+# until that direction is no longer possible. Then make a random
+# choice.
+class RandomishAgent(Agent):
+
+    # Constructor
+    #
+    # Create a variable to hold the last action
+    def __init__(self):
+         self.last = Directions.STOP
+    
+    def getAction(self, state):
+        # Get the actions we can try, and remove "STOP" if that is one of them.
+        legal = api.legalActions(state)
+        if Directions.STOP in legal:
+            legal.remove(Directions.STOP)
+        # If we can repeat the last action, do it. Otherwise make a
+        # random choice.
+        if self.last in legal:
+            return api.makeMove(self.last, legal)
+        else:
+            pick = random.choice(legal)
+            # Since we changed action, record what we did
+            self.last = pick
+            return api.makeMove(pick, legal)
+
+#GoWestAgent
+#Always tries to have Pacman go west on the grid when it is possible
+#If go west is not an option, go up
+class GoWestAgent(Agent):
+
+    #Create a variable to hold the last action
+    def __init__(self):
+        self.last = Directions.STOP
+        pick = Directions.STOP
+    def getAction(self, state):
+        #Get the actions we can try
+        legal = api.legalActions(state)
+        if Directions.STOP in legal:
+            legal.remove(Directions.STOP)
+        #Check if West is available, if it is, do it. Otherwise continue the same action. 
+        if Directions.WEST in legal:
+            return api.makeMove(Directions.WEST, legal)
+        # If we can repeat the last action, do it. Otherwise make a
+        # random between up or down choice.
+        elif self.last in legal:
+            return api.makeMove(self.last, legal) 
+        else:
+            if Directions.NORTH and Directions.SOUTH in legal:
+                upOrDown = (Directions.NORTH, Directions.SOUTH)
+                pick = random.choice(upOrDown)
+            else:
+                pick = random.choice(legal)
+            #Change last action
+            self.last = pick
+            return api.makeMove(pick, legal)
+
+#Hungry Agent
+#Uses information about the location of the food to try to move towards the nearest food
+class HungryAgent(Agent):
+    #Create a variable to hold food locations and self location
+    def __init__(self):
+        location = api.whereAmI(state)
+        foods = api.food(state)
+    def getAction(self, state):
+        
+
+# SensingAgent
+#
+# Doesn't move, but reports sensory data available to Pacman
+class SensingAgent(Agent):
+
+    def getAction(self, state):
+
+        # Demonstrates the information that Pacman can access about the state
+        # of the game.
+
+        # What are the current moves available
+        legal = api.legalActions(state)
+        print "Legal moves: ", legal
+
+        # Where is Pacman?
+        pacman = api.whereAmI(state)
+        print "Pacman position: ", pacman
+
+        # Where are the ghosts?
+        print "Ghost positions:"
+        theGhosts = api.ghosts(state)
+        for i in range(len(theGhosts)):
+            print theGhosts[i]
+
+        # How far away are the ghosts?
+        print "Distance to ghosts:"
+        for i in range(len(theGhosts)):
+            print util.manhattanDistance(pacman,theGhosts[i])
+
+        # Where are the capsules?
+        print "Capsule locations:"
+        print api.capsules(state)
+        
+        # Where is the food?
+        print "Food locations: "
+        print api.food(state)
+
+        # Where are the walls?
+        print "Wall locations: "
+        print api.walls(state)
+        
+        # getAction has to return a move. Here we pass "STOP" to the
+        # API to ask Pacman to stay where they are.
+        return api.makeMove(Directions.STOP, legal)
